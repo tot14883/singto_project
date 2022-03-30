a86b74d74d43e5b7dd3ba24c4e2680f17ae524e3
tot14883 <tot14883@gmail.com>
2022-03-30 23:01:58 +0700

    Add code example
    30/3/2565 23:01

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
+	<link rel="stylesheet" href="css/index.css">
+
 	<title>Class room</title>
 
 	<style>
-		.color-red {
-			background-color: red;
-		}
-
-		.text-header {
-
+		.nav-bar {
+			background-color: var(--primary);
+			border-bottom: 1px solid var(--blue-ocean);
 		}
 	</style>
+
 </head>
 <body>
 	<div>
-		<div class="d-flex text-center">
-			<div class="flex-grow-1 text-header color-red">Flex item</div>
+		<div class="d-flex text-center nav-bar">
+			<div class="flex-grow-1 text-nav-bar">Classroom</div>
 		</div>
 	</div>
 </body>
