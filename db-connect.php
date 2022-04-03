468747f6054ab46aa373560e6519dce405de3a51
Chayangkoon Tona <chayangkoontona@gmail.com>
2022-04-03 21:17:53 +0700

    fix css

--- db-connect.php
+++ db-connect.php
@@ -1,4 +1,4 @@
 <?php 
-   $con = mysqli_connect("localhost", "root", "", "student_classroom_project");
+    $con = mysqli_connect("localhost", "root", "", "student_classroom_project");
     mysqli_set_charset($con, "utf8");
 ?>
